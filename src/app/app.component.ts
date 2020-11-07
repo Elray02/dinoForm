3e583cf1054732824362c7729cafbffd9d65726e
elray02 <tassofede@gmail.com>
2020-11-07 19:42:52 +0100

    test that all is working as expected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Subject } from 'rxjs';
+import { UserService } from './UserService.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dinoForm';
   public Editor = ClassicEditor;
+  userNickName: Subject<string>;
+  constructor(private userservice: UserService) {}
+  ngOnInit(): void {
+    this.userNickName = this.userservice.userName;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { UserNameComponent } from './userName/userName.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [	AppComponent,
+      UserNameComponent
+   ],
   imports: [BrowserModule, NgbModule, CKEditorModule],
   providers: [],
   bootstrap: [AppComponent],
