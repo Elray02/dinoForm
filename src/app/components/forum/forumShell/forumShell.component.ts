ed89de086d8144839fe4c7c4387cc477b97ca4cb
elray02 <tassofede@gmail.com>
2020-11-11 11:33:50 +0100

    add comments

--- src/app/components/forum/forumShell/forumShell.component.ts
+++ src/app/components/forum/forumShell/forumShell.component.ts
@@ -12,10 +12,12 @@ export class ForumShellComponent implements OnInit {
   postList$: Observable<PostModel[]>;
 
   constructor(private postService: PostService) {
+    // call the state for populate the list with some fake value
     this.postService.initializePost();
   }
 
   ngOnInit() {
+    // get the new created list
     this.postList$ = this.postService.postState.asObservable();
   }
 }
