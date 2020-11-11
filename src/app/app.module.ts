8c8031e72ccd4c136e0989e706236a9177f0acb9
elray02 <tassofede@gmail.com>
2020-11-11 11:27:20 +0100

    import the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { UserNameComponent } from './components/userName/userName.component';
 import { PostListComponent } from './components/forum/post-list/post-list.component';
+import { ForumShellComponent } from './components/forum/forumShell/forumShell.component';
+import { PostDetailComponent } from './components/forum/post-detail/post-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CommentComponent } from './components/forum/comment/comment.component';
 
 @NgModule({
-  declarations: [AppComponent, UserNameComponent, PostListComponent],
-  imports: [BrowserModule, NgbModule, CKEditorModule],
+  declarations: [
+    AppComponent,
+    UserNameComponent,
+    PostListComponent,
+    ForumShellComponent,
+    PostDetailComponent,
+    CommentComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    NgbModule,
+    CKEditorModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
